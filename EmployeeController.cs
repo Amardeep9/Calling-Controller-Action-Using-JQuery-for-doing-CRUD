78c6e89a0e437f611ce8bbfd66005db959cd68dc
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-08-10 12:43:58 +0530

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,126 @@
+﻿using CrudUsingJqueryUsingADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudUsingJqueryUsingADO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            lstEmployee = objemployee.GetAllEmployees().ToList();
+           
+            return View(lstEmployee);
+        }
+        
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                objemployee.AddEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+
+            if (employee == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            return View(employee);
+        }
+
+        [HttpPost]// submit operation
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind]Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            if (ModelState.IsValid)
+            {
+                objemployee.UpdateEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+
+            if (employee == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            return View(employee);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+
+            if (employee == null)
+            {
+                return new HttpNotFoundResult("Not Found");
+            }
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            objemployee.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
